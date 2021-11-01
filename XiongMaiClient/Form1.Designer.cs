2ea78da5c17db9ac694982e1d43abd822db44c5f
buzz1k <default-sl@yandex.ru>
2021-11-02 00:48:11 +0700

    add device control

--- XiongMaiClient/Form1.Designer.cs
+++ XiongMaiClient/Form1.Designer.cs
@@ -28,12 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.deviceList1 = new XiongMaiClient.Controlls.DeviceList();
+            this.SuspendLayout();
+            // 
+            // deviceList1
+            // 
+            this.deviceList1.Location = new System.Drawing.Point(720, 12);
+            this.deviceList1.Name = "deviceList1";
+            this.deviceList1.Size = new System.Drawing.Size(202, 436);
+            this.deviceList1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(934, 636);
+            this.Controls.Add(this.deviceList1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Controlls.DeviceList deviceList1;
     }
 }
